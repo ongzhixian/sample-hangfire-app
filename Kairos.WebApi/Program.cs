0b0e7df8f855c18fe46cb3324612fba932e402ef
ongzhixian <zhixian@hotmail.com>
2025-10-07 23:05:46 +0800

    Add Hangfire

--- Kairos.WebApi/Program.cs
+++ Kairos.WebApi/Program.cs
@@ -1,5 +1,7 @@
 using Kairos.WebApi.ApiOperations;
 using Serilog;
+using Hangfire;
+using Hangfire.SqlServer;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -14,6 +16,14 @@ try
     builder.Services.AddSerilog((_, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
 
     builder.Services.AddSwaggerGen();
+    
+    builder.Services.AddHangfire(configuration => configuration
+        .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+        .UseSimpleAssemblyNameTypeSerializer()
+        .UseRecommendedSerializerSettings()
+        .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection")));
+    
+    builder.Services.AddHangfireServer();
     
     builder.Services.AddHealthChecks();
     
@@ -34,7 +44,9 @@ try
     app.UseDefaultFiles();
     
     app.UseStaticFiles();
-
+    
+    app.UseHangfireDashboard();
+    
     app.MapHealthChecks("health");
     
     app.MapHelloWorldApi();
